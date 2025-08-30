00b8c573fcb25ab95d44d5ed8733c84b536a6ea3
abdalla mansour <a.mansour.code@gmail.com>
2025-08-30 18:17:31 +0300

    Add test route for FCM notifications in web.php. Implemented a new route to send test notifications using FCMService, enhancing testing capabilities for push notifications.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\FCMService;
 
-
+Route::get('test-fcm/{id}', function ($id) {
+    $fcmService = new FCMService();
+    $fcmService->sendTopicNotification($id, 'Test Title', 'Test Message');
+    return 'Test FCM';
+});
